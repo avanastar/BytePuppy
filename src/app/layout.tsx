b2884e6cf37e7c887127f49664005b791ef8a768
Ava <avanastar3@gmail.com>
2025-02-05 19:49:00 -0400

    🎨 Add icons and enhance metadata
    - Added apple-touch-icon and og-image files.
    - Updated layout metadata with viewport settings, theme color, and Open Graph data.
    - Included Twitter card info and robots directives for better SEO.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,47 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 export const metadata: Metadata = {
   title: "BytePuppy",
   description: "BytePuppy is a project by Ava",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  themeColor: "#000000",
+
+  openGraph: {
+    title: "BytePuppy",
+    description: "BytePuppy is a project by Ava",
+    url: "https://bytepuppy.dev",
+    siteName: "BytePuppy",
+    images: [{ url: "/og-image.png" }],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "BytePuppy",
+    description: "BytePuppy is a project by Ava",
+    images: [{ url: "/og-image.png" }],
+  },
+
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      "max-video-preview": 0,
+      "max-image-preview": "large",
+      "max-snippet": 0,
+    },
+  },
+  category: "social",
+  creator: "Ava Nastar",
+  icons: [
+    { rel: "icon", url: "/favicon.ico" },
+    { rel: "apple-touch-icon", url: "/apple-touch-icon.png" },
+  ],
 };
 
 export default function RootLayout({
